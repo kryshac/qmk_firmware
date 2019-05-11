aec69e5b613841537705ebbb18b1d455e6bb5965
Cristian Cazacu <vladislav.tornea@gmail.com>
2019-05-11 23:34:15 +0300

    change layout

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/kryshac/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/kryshac/keymap.c
@@ -1,4 +1,4 @@
-/* A standard layout for the Dactyl Manuform 5x6 Keyboard */ 
+)/* A standard layout for the Dactyl Manuform 5x6 Keyboard */ 
 
 #include QMK_KEYBOARD_H
 
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|               |------+------+------+------+------+------|
  * |  esc |   a  |   s  |   d  |   f  |   g  |               |   h  |   j  |   k  |   l  |  ; : |  ' " |
  * |------+------+------+------+------+------|               |------+------+------+------+------+------|
- * | ctrl |   z  |   x  |   c  |   v  |   b  |               |   n  |   m  |  , < |  . > |  / ? |  \ | |
+ * | caps |   z  |   x  |   c  |   v  |   b  |               |   n  |   m  |  , < |  . > |  / ? |  \ | |
  * ,------+------+------+--------------------,               ,--------------------+------+------+------,
- *               |  up  | down |                                           |  [ { | ] }  |
+ *               |    ( | )    |                                           |  [ { | ] }  |
  *               '------+------'------'                             '------'------+------'
  *                      | raise|space |                             | enter|lower |
  *                      '------+------'                             '------+------'
  *                             '------+------'               '------+------'
- *                             | shift| left |               | right|shift |
+ *                             | shift| ctrl |               | ctrl |shift |
  *                             '------+------'               '------+------'
  *                             |  bs  | del  |               |  gui | alt  |
  *                             '------+------'               '------+------'
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV , KC_1  , KC_2   , KC_3  , KC_4   , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_EQL ,
      KC_TAB , KC_Q  , KC_W   , KC_E  , KC_R   , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
      KC_ESC , KC_A  , KC_S   , KC_D  , KC_F   , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X   , KC_C  , KC_V   , KC_B  ,                         KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_BSLS,
-                      KC_DOWN, KC_UP ,                                                         KC_LBRC,KC_RBRC,
+     KC_CAPS, KC_Z  , KC_X   , KC_C  , KC_V   , KC_B  ,                         KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_BSLS,
+                      KC_LSPO,KC_RSPC,                                                         KC_LBRC,KC_RBRC,
                                RAISE , KC_SPC,                                         KC_ENT , LOWER ,
-                                       KC_LSFT,KC_LEFT,                        KC_RGHT,KC_RSFT,
+                                       KC_LSFT,KC_LCTL,                        KC_RCTL,KC_RSFT,
                                        KC_BSPC, KC_DEL,                        KC_LGUI,KC_LALT
   ),
 
