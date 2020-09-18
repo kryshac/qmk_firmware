cb690707d23b1589bfac099a8462a816f5c78372
Cristian Cazacu <ccazacu@popnlink.com>
2020-09-18 20:18:00 +0300

    update keyboard layout

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/kryshac/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/kryshac/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|               |------+------+------+------+------+------|
  * |  esc |   z  |   x  |   c  |   v  |   b  |               |   n  |   m  |  , < |  . > |  / ? |  \ | |
  * ,------+------+------+--------------------,               ,--------------------+------+------+------,
- *               |  ` ~ |   $  |                                           |  [ { | ] }  |
+ *               |  ` ~ |   $  |                                           |  = + | - _  |
  *               '------+------'------'                             '------'------+------'
  *                      | raise|space |                             | enter|lower |
  *                      '------+------'                             '------+------'
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_Q  , KC_W   , KC_E  , KC_R   , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
      KC_CAPS, KC_A  , KC_S   , KC_D  , KC_F   , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_ESC , KC_Z  , KC_X   , KC_C  , KC_V   , KC_B  ,                         KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_BSLS,
-                      KC_GRV ,KC_DLR ,                                                         KC_LBRC,KC_RBRC,
+                      KC_GRV ,KC_DLR ,                                                         KC_EQL ,KC_MINS,
                                RAISE , KC_SPC,                                         KC_ENT , LOWER ,
                                        KC_LCTL,KC_LSFT,                        KC_RSFT,KC_RCTL,
                                        KC_BSPC, KC_DEL,                        KC_LGUI,KC_LALT
